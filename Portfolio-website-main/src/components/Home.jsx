90b0afe9154e3d971af9ffe83284c248319c80ff
Siddharaj Mali <121725799+sid18003@users.noreply.github.com>
2024-10-17 20:10:54 +0530

    Update Home.jsx

--- Portfolio-website-main/src/components/Home.jsx
+++ Portfolio-website-main/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Dp from "../assets/dp2.png";
+import Dp from "../assets/insta_upload.jpg";
 import { MdOutlineArrowRightAlt } from 'react-icons/md';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
